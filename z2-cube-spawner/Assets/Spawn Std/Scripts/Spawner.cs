d6275f2e7c27146bc0357246111f2e1b84416447
Faraz Jamshed <93920080+farazjam@users.noreply.github.com>
2022-04-19 13:25:29 +0500

    Standard spawner complete

---
+++ z2-cube-spawner/Assets/Spawn Std/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cube.Spawner.Standard
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] GameObject cubePrefab;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonUp(0)) Spawn();
+        }
+
+        void Spawn()
+        {
+            GameObject cube = Instantiate(cubePrefab);
+
+        }
+    }
+}
